34c3dc40a6890dfaeb8f469f620c22839f7d650e
rokasramas <rokasramas@gmail.com>
2018-11-07 23:47:52 +0200

    updated docstrings

--- tokenizer/tokenizer.py
+++ tokenizer/tokenizer.py
@@ -9,27 +9,27 @@ TAGGER.open('resources/lt-model.crfsuite')
 
 
 def homogeneous(string: str) -> re.match:
-    """determine if string is made up of similar types of characters"""
+    """determines if string is made up of similar types of characters"""
     return re.match("(^\p{L}+$|^\p{Z}+$|^\p{N}+$|^\.+$|^\[?!]+$|^\,+$)", string)
 
 
 def get_feature(to_left: str, to_right: str) -> List[str]:
-    """generate single feature given string to left and right"""
+    """generates single feature given string to left and right"""
     return ['h:1'] if homogeneous(to_left[-1:]+to_right[:1]) else list(chain(
         (f'c{-i}:{char}' for char, i in zip(to_left, reversed(range(1, len(to_left)+1)))),
         (f'c{i}:{char}' for char, i in zip(to_right, range(len(to_right))))
     ))
 
 
-def apply_labels(tags: str, string: str) -> Iterator[str]:
-    """split string at every position, where there is '1'"""
+def apply_labels(labels: str, string: str) -> Iterator[str]:
+    """splits string at every label position equal to '1'"""
     return (
-        string[begin:end] for begin, end in (instance.span() for instance in re.finditer('10*', tags))
+        string[begin:end] for begin, end in (instance.span() for instance in re.finditer('10*', labels))
     )
 
 
 def get_features(string: str, start: int, finish: int, left_span: int, right_span: int) -> Iterator[List[str]]:
-    """prepare string features in given range"""
+    """prepares string features in given range"""
     return (
         get_feature(string[max(0, index-left_span):index], string[index:index+right_span])
         for index in range(start, finish)
@@ -37,7 +37,7 @@ def get_features(string: str, start: int, finish: int, left_span: int, right_spa
 
 
 def tag(string: str, tagger: pycrfsuite.Tagger, start: int, finish: int, left_span: int, right_span: int) -> str:
-    """splits chunk into tokens"""
+    """predicts labels in given range"""
     return '1' + ''.join(
         tagger.tag(list(get_features(string, start+1, finish, left_span, right_span)))
     )
